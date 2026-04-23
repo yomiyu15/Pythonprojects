1793b87615758de43aca33754bbe37335e24d631
Yomiyu15 <yomiwaaq@gmail.com>
2026-04-23 15:17:16 +0300

    first commit: including calculator script

---
+++ hello.py
@@ -0,0 +1,9 @@
+a = float(input("Enter the first number :"))
+b = float(input("Enter the second number"))
+
+print ("Sum of the two number is:",a+b)
+print("Difference of the two number is:",a-b)
+print ("Product of the two number is ",a*b)
+print("Quotient of the two number is ",a/b)
+
+
